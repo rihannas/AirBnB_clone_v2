743fa8d563a2f212150c4c8070f5cf045866023f
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-03-20 00:24:20 +0300

    set up database & priviliges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED  BY 'hbnb_dev_pwd';
+
+
+-- set priviliges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
