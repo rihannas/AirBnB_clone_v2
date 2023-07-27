145b57f3d1b8c20ac1cd94f3597194f254197204
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-07-27 05:56:37 +0300

    updated state class

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
@@ -11,3 +12,15 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship(City, backref='state', cascade='all, delete')
+
+    if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+        @property
+        def cities(self):
+            """ returns the list of City instances with state_id equals to the current State.id => It will be the FileStorage relationship between State and City"""
+            city_list = []
+            from models import storage
+            from models.city import City
+            for key, value in storage.all(City).items():
+                if (self.id == value.state_id):
+                    city_list.append(value)
+            return city_list
